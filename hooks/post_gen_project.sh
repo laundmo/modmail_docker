19348e28cc1874ce110787d9aadb7aa209224b97
laundmo <laurinschmidt2001@gmail.com>
2020-06-25 00:15:16 +0200

    feat: initial commit

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git clone https://github.com/kyb3r/modmail
+cd modmail
+docker build . --tag=modmail
+cd ..
+git clone https://github.com/laundmo/logviewer
+cd logviewer
+git checkout patch-1
+docker build . --tag=logviewer
+cd ..
+docker-compose up -d
