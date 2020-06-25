451c0c5bd2ba49743d22c8b0f0718747fbf0167b
laundmo <laurinschmidt2001@gmail.com>
2020-06-25 03:25:35 +0200

    Create post_gen_project.sh

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/kyb3r/modmail
+cd modmail
+docker build . --tag=modmail
+cd ..
+git clone https://github.com/laundmo/logviewer
+cd logviewer
+git checkout patch-1
+docker build . --tag=logviewer
+cd ..
