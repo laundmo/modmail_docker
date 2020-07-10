482023512f1f6b39bb1be8e3703d3a2294ddb987
laundmo <laurinschmidt2001@gmail.com>
2020-07-10 10:25:03 +0200

    build by dockerfile instead

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 git clone https://github.com/kyb3r/modmail
-cd modmail
-docker build . --tag=modmail
-cd ..
 git clone https://github.com/laundmo/logviewer
-cd logviewer
-git checkout patch-1
-docker build . --tag=logviewer
-cd ..
+docker-compose build
 docker-compose up -d
